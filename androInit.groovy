785b68a224e57b2b44f0fa6d25a6b6ca680ac85a
Meyrueixo <helixx@ymail.com>
2016-10-06 16:39:42 +0200

    Update androInit.groovy

--- androInit.groovy
+++ androInit.groovy
@@ -1,7 +1,12 @@
 job('Andro-Init') {
   steps {
-      shell('sudo apt-get update')
+	  shell('
+	  shell('sudo apt-get update')
 	  shell('sudo apt-get install -y lib32z1')
 	  shell('sudo apt-get install -y gcc-multilib')
+	  shell('sudo dpkg --add-architecture i386')
+	  shell('sudo apt-get -qqy update')
+	  shell('sudo apt-get -qqy install libncurses5:i386 libstdc++6:i386 zlib1g:i386')
+	  shell('sudo apt-get install -y libqt5webkit5')
   }
 }
